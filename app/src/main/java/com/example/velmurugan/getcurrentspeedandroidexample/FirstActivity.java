93f15d476c9ab98b811a41bd8af8593dd5658d35
Iftekhar Alam Tuhin <iftekharbd2@gmail.com>
2018-04-09 15:00:20 +0600

    Fixing Complete

--- app/src/main/java/com/example/velmurugan/getcurrentspeedandroidexample/FirstActivity.java
+++ app/src/main/java/com/example/velmurugan/getcurrentspeedandroidexample/FirstActivity.java
@@ -9,20 +9,36 @@ import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
 
-    EditText etFixedPrice,etChargePerKm,etWaitingCharge;
+    EditText etFixedPrice, etChargePerKm, etWaitingCharge;
     Button btSet;
+    int baseFareI, chargePerKmI, waitChargeI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        btSet=findViewById(R.id.btSet);
+        btSet = findViewById(R.id.btSet);
+        etFixedPrice = findViewById(R.id.etFixedPrice);
+        etChargePerKm = findViewById(R.id.etChargePerKm);
+        etWaitingCharge = findViewById(R.id.etWaitingCharge);
+
 
         btSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                Intent MainAct = new Intent(FirstActivity.this, MainActivity.class);
+//                startActivity(MainAct);
                 Intent MainAct = new Intent(FirstActivity.this, MainActivity.class);
+                //intent.putExtra ( "TextBox", editText.getText().toString() );
+
+                baseFareI = Integer.parseInt(etFixedPrice.getText().toString());
+                chargePerKmI = Integer.parseInt(etChargePerKm.getText().toString());
+                waitChargeI = Integer.parseInt(etWaitingCharge.getText().toString());
+
+                MainAct.putExtra("baseFareI", baseFareI);
+                MainAct.putExtra("chargePerKmI", chargePerKmI);
+                MainAct.putExtra("waitChargeI", waitChargeI);
                 startActivity(MainAct);
             }
         });
--- app/src/main/java/com/example/velmurugan/getcurrentspeedandroidexample/MainActivity.java
+++ app/src/main/java/com/example/velmurugan/getcurrentspeedandroidexample/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.velmurugan.getcurrentspeedandroidexample;
 
 import java.math.BigDecimal;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -23,14 +26,38 @@ public class MainActivity extends Activity implements GPSCallback{
     double currentSpeed,kmphSpeed,currentDistance;
     EditText etFixedPrice,etWaitingCharge,etRunningCost;
     private TextView txtview;
+
+    long timeSwap = 0L;
+    long timeInMillies = 0L;
+    private long startTime = 0L;
+    long finalTime = 0L;
+    int minutes,seconds;
+    private TextView textTimer;
+    private TextView textFare;
+
     private Location prevLocation;
     private double distance = 0.0;
+    private double distance1 = 0.0;
     private TextView distance_textView;
+    private Handler myHandler = new Handler();
+
+    int baseFareR;
+    int chargePerKmR;
+
+    int waitChargeR;
+
+    //VarTimer
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        baseFareR= getIntent().getExtras().getInt("baseFareI");
+        chargePerKmR=getIntent().getExtras().getInt("chargePerKmI");
+        waitChargeR=getIntent().getExtras().getInt("waitChargeI");
+
 
         try {
             if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
@@ -42,6 +69,8 @@ public class MainActivity extends Activity implements GPSCallback{
 
         txtview = findViewById(R.id.info);
         distance_textView = findViewById(R.id.infoDistance);
+        textTimer = findViewById(R.id.textTimer);
+        textFare=findViewById(R.id.textFare);
 //        etFixedPrice=findViewById(R.id.etFixedPrice);
 //        etWaitingCharge=findViewById(R.id.etWaitingCharge);
 //        etRunningCost=findViewById(R.id.etChargePerKm);
@@ -72,18 +101,10 @@ public class MainActivity extends Activity implements GPSCallback{
         try {
             double distanceToLast = location.distanceTo(prevLocation);
             //if less than 10 metres, do not record
-            if (distanceToLast < 10.00) {
-               distance_textView.setText(distance+"Km");
-            } else{
-                distance += distanceToLast;
-
-
-                ///
-
-
-                //Toast.makeText(this, distance + "", Toast.LENGTH_LONG).show();
-                distance_textView.setText(distance+"Km");
-            }
+            distance += distanceToLast;
+            distance1 = distance / 1000;
+            //Toast.makeText(this, distance + "", Toast.LENGTH_LONG).show();
+            distance_textView.setText(distance1+"Km");
             prevLocation = location;
         }catch (NullPointerException ex){
             prevLocation = location;
@@ -96,32 +117,34 @@ public class MainActivity extends Activity implements GPSCallback{
 
     @Override
     public void onGPSUpdate(Location location) {
-        updateDistance(location);
-        speed = location.getSpeed();
-        currentSpeed = round(speed,3,BigDecimal.ROUND_HALF_UP);
-        kmphSpeed = round((currentSpeed*3.6),3,BigDecimal.ROUND_HALF_UP);
+
+        if (location != null) {
+            updateDistance(location);
+            speed = location.getSpeed();
+            currentSpeed = round(speed,3,BigDecimal.ROUND_HALF_UP);
+            kmphSpeed = round((currentSpeed*3.6),3,BigDecimal.ROUND_HALF_UP);
+        }
+        //fff
+        distance1= round(distance1,3,BigDecimal.ROUND_HALF_UP);
 
         //time
 
         txtview.setText(kmphSpeed+"km/h");
+        startTime = SystemClock.uptimeMillis();
 
-        Log.d("Status", "Hi");
+        if (kmphSpeed == 0.0) {
+            timeSwap += timeInMillies;
+            myHandler.postDelayed(updateTimerMethod, 0);
+        } else {
+            startTime = SystemClock.uptimeMillis();
+            myHandler.removeCallbacks(updateTimerMethod);
+        }
+        //Log.d("Status", "Hi");
+        totalCost=baseFareR + distance1*chargePerKmR + minutes*waitChargeR + (seconds/60)*waitChargeR;
+        textFare.setText(String.format("%s", totalCost));
     }
 
-    private double getRunningCost() {
 
-
-
-        return 0;
-    }
-
-    private double getTotalWaitingTime(double kmphSpeed) {
-
-        
-
-
-        return 0;
-    }
 
     @Override
     protected void onDestroy() {
@@ -136,4 +159,20 @@ public class MainActivity extends Activity implements GPSCallback{
         BigDecimal rounded = bd.setScale(precision, roundingMode);
         return rounded.doubleValue();
     }
+
+    private Runnable updateTimerMethod = new Runnable() {
+
+        public void run() {
+            timeInMillies = SystemClock.uptimeMillis() - startTime;
+            finalTime = timeSwap + timeInMillies;
+
+            seconds = (int) (finalTime / 1000);
+            minutes = seconds / 60;
+            seconds = seconds % 60;
+            int milliseconds = (int) (finalTime % 1000);
+
+            textTimer.setText(String.format("%d:%s:%s", minutes, String.format("%02d", seconds),String.format("%03d",milliseconds)));
+            myHandler.postDelayed(this, 0);
+        }
+    };
 }
